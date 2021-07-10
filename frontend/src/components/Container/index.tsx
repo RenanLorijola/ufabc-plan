3666718ea0bd8cc69bfce6da08982450c5ed61d7
RenanZL <renanzago@gmail.com>
2021-07-10 01:37:17 -0300

    frontend: criação de componente de container

---
+++ frontend/src/components/Container/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+  CssBaseline,
+  Container
+} from '@material-ui/core';
+
+const ContainerComponent: React.FC = ({children}): JSX.Element => {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container fixed>
+        {children}
+      </Container>
+    </React.Fragment>
+  )
+}
+
+export default ContainerComponent
