ddd1a2056d227566d232fd8bbe27018dae9b6b52
RenanZL <renanzago@gmail.com>
2021-07-10 02:14:40 -0300

    frontend: correção position header

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -4,10 +4,10 @@ import { AppBar, Box, Toolbar, Typography, Button } from '@material-ui/core'
 const Header: React.FC = (): JSX.Element => {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="sticky">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            Logo
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
